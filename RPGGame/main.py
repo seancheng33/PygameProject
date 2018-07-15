fdc05673a4ea59584ae72136e875bf592a6c9982
seancheng <aya234@163.com>
2018-07-15 21:13:51 +0800

    rpg游戏开始

---
+++ RPGGame/main.py
@@ -0,0 +1,17 @@
+'''
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2018/7/15
+@Program      : 游戏的主入口
+'''
+import pygame
+
+pygame.init()
+SCREEN = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption('RPG游戏的测试demo')
+
+while True:
+
+
+
+    pygame.display.update()
