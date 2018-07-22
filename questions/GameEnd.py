98e8afd8afa69b117271df05df40f58bd2af617e
seancheng <aya234@163.com>
2018-07-22 15:07:50 +0800

    GameEnd重构文字和按键事件完成

--- questions/GameEnd.py
+++ questions/GameEnd.py
@@ -10,20 +10,19 @@ from pygame.locals import *
 
 def end_game(SURFACE, globalVar, score, right_score, totalNum):
     # 游戏的结束界面，对刚才完过的游戏做出一些数据统计。
-    score_img = globalVar.globalFont.render('最终总成绩： ' + str(score), True, globalVar.color_dict['orange'])
-    score_rect = score_img.get_rect()
+    score_img, score_rect = globalVar.maketext(globalVar.globalFont, '最终总成绩： ' + str(score),
+                                               globalVar.color_dict['orange'])
     score_rect.top = 150
     score_rect.centerx = SURFACE.get_rect().centerx
 
-    total_right_percent = right_score / totalNum
-    right_img = globalVar.globalFont.render('正确率： ' + str(total_right_percent * 100) + ' %', True,
-                                            globalVar.color_dict['orange'])
-    right_rect = right_img.get_rect()
+    total_right_percent = right_score / totalNum * 100
+    right_img, right_rect = globalVar.maketext(globalVar.globalFont, '正确率： ' + str(total_right_percent) + ' %',
+                                               globalVar.color_dict['orange'])
     right_rect.top = 200
     right_rect.centerx = SURFACE.get_rect().centerx
 
-    right_score_img = globalVar.globalFont.render('正确题数： ' + str(right_score), True, globalVar.color_dict['orange'])
-    right_score_rect = right_score_img.get_rect()
+    right_score_img, right_score_rect = globalVar.maketext(globalVar.globalFont, '正确题数： ' + str(right_score),
+                                                           globalVar.color_dict['orange'])
     right_score_rect.top = 250
     right_score_rect.centerx = SURFACE.get_rect().centerx
 
@@ -38,15 +37,13 @@ def end_game(SURFACE, globalVar, score, right_score, totalNum):
     bt_back = pygame.Rect((0, 0), (180, 60))
     bt_back.centerx = SURFACE.get_rect().centerx - 120
     bt_back.bottom = 580
-    back_img = globalVar.globalFont.render('返回标题', True, globalVar.color_dict['white'])
-    back_rect = back_img.get_rect()
+    back_img, back_rect = globalVar.maketext(globalVar.globalFont, '返回标题', globalVar.color_dict['white'])
     back_rect.center = bt_back.center
 
     bt_exit = pygame.Rect((0, 0), (180, 60))
     bt_exit.centerx = SURFACE.get_rect().centerx + 120
     bt_exit.bottom = 580
-    exit_img = globalVar.globalFont.render('退出游戏', True, globalVar.color_dict['white'])
-    exit_rect = exit_img.get_rect()
+    exit_img, exit_rect = globalVar.maketext(globalVar.globalFont, '退出游戏', globalVar.color_dict['white'])
     exit_rect.center = bt_exit.center
 
     while True:
@@ -66,20 +63,20 @@ def end_game(SURFACE, globalVar, score, right_score, totalNum):
             elif event.type == KEYUP:
                 if event.key == K_ESCAPE:
                     globalVar.close_program()
-                elif event.key == K_RETURN:
-                    return 'reset'
+                # elif event.key == K_RETURN:
+                #     return 'reset'
         pygame.mouse.set_visible(True)
 
         x, y = pygame.mouse.get_pos()
         pressed = pygame.mouse.get_pressed()
 
-        if bt_back.left < x < bt_back.right and bt_back.top < y < bt_back.bottom:
+        if bt_back.collidepoint(x, y):
             pygame.draw.rect(SURFACE, globalVar.color_dict['lime'], bt_back)
             for event in pressed:
                 if event == 1:
                     return 'reset'
 
-        if bt_exit.left < x < bt_exit.right and bt_exit.top < y < bt_exit.bottom:
+        if bt_exit.collidepoint(x, y):
             pygame.draw.rect(SURFACE, globalVar.color_dict['lime'], bt_exit)
             for event in pressed:
                 if event == 1:
