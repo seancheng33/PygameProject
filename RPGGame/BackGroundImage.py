9913cb42900be256ef87c3ca292753843b78bdb0
seancheng <aya234@163.com>
2018-08-12 22:18:46 +0800

    开始RPG游戏的功能制作测试

---
+++ RPGGame/BackGroundImage.py
@@ -0,0 +1,6 @@
+'''
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2018/8/12
+@Program      : 这里是用来加载背景图片的,（预留）
+'''
--- RPGGame/main.py
+++ RPGGame/main.py
@@ -10,6 +10,12 @@ pygame.init()
 SCREEN = pygame.display.set_mode((1024, 768))
 pygame.display.set_caption('RPG游戏的测试demo')
 
+IMAGEDICTS = {'road': pygame.image.load('img/city/road01.png'),
+              'grass': pygame.image.load('img/city/grass01.png'),
+              'seawall': pygame.image.load('img/city/seawall01.png'),
+              'sea': pygame.image.load('img/city/sea.png'),
+              }
+
 while True:
 
 
