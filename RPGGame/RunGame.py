acc176b1ed9cd1f228fd4184a87fe5eb4ab478b8
seancheng33 <seancheng33@gmail.com>
2018-08-15 14:53:56 +0800

    微调代码结构

---
+++ RPGGame/RunGame.py
@@ -0,0 +1,68 @@
+'''
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2018/8/15
+@Program      : 运行游戏的函数
+'''
+import pygame
+from pygame.locals import *
+from RPGGame import DrawMap, mapdata
+from RPGGame.GlobalSetting import GlobalSetting
+from RPGGame.sysfunction import can_move
+
+setting = GlobalSetting()
+
+def run_map(mapList, gameStateObj):
+    currentMapName = gameStateObj['map name']
+    mapObj = mapList[currentMapName]
+    showMenu = False
+
+    while True:
+        setting.SCREENFACE.fill(setting.COLORDICT['bgcolor'])
+
+        map = DrawMap.draw_map(mapObj, gameStateObj)
+        mapRect = map.get_rect()
+        mapRect.left = 0
+        mapRect.top = 0
+        map = DrawMap.redraw_map(map, gameStateObj)
+
+        setting.SCREENFACE.blit(map, mapRect)
+        x, y = gameStateObj['player']
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return 'exit'
+            elif event.type == KEYDOWN:
+                # 控制角色的移动
+                if event.key == K_LEFT or event.key == K_a:
+                    if x > 0:
+                        gameStateObj['player'] = can_move(mapObj, x, y, setting.DESC['LEFT'])
+                        # gameStateObj['player'] = (x - 1, y)
+                elif event.key == K_RIGHT or event.key == K_d:
+                    if x < len(mapObj[0])-1:
+                        gameStateObj['player'] = can_move(mapObj, x, y, setting.DESC['RIGHT'])
+                        # gameStateObj['player'] = (x + 1, y)
+                elif event.key == K_UP or event.key == K_w:
+                    if y > 0:
+                        gameStateObj['player'] = can_move(mapObj, x, y, setting.DESC['UP'])
+                        # gameStateObj['player'] = (x, y - 1)
+                elif event.key == K_DOWN or event.key == K_s:
+                    if y < len(mapObj)-1:
+                        gameStateObj['player'] = can_move(mapObj, x, y, setting.DESC['DOWN'])
+                        # gameStateObj['player'] = (x, y + 1)
+
+
+                elif event.key == K_ESCAPE:
+                    return 'exit'
+                elif event.key == K_BACKSPACE:
+                    return 'title'
+
+        for item in mapdata.inter_postion[currentMapName].items():
+            if (x, y) == item[1]:
+                currentMapName = item[0][2:]
+                gameStateObj['map name'] = currentMapName
+                gameStateObj['player'] = mapdata.role_postion[currentMapName]
+                mapObj = mapList[currentMapName]
+
+        pygame.display.update()
+        setting.FPSCLOCK.tick(60)
---
+++ RPGGame/npc.py
@@ -0,0 +1,6 @@
+'''
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2018/8/15
+@Program      : 自己活动的NPC的功能实现。
+'''
