097fb41d4e00e8392aebba1ff7dcc1f94d91ef5f
seancheng33 <seancheng33@gmail.com>
2018-08-16 09:17:42 +0800

    微调地图

--- RPGGame/mapdata.py
+++ RPGGame/mapdata.py
@@ -11,7 +11,7 @@ role_postion = {'base': (10, 12),
                 }
 
 # 地图的入口和出口 可以有多个，比如一个城市的出入口可以是出入到大地图、各种商店、隐藏剧情的房子等等，一个字典中包含多个字典来控制
-inter_postion = {'base': {'tosea': (10, 10), 'itemshop': (5, 5), 'bank': (15, 20)},
+inter_postion = {'base': {'tosea': (10, 10), 'toitemshop': (5, 5), 'tobank': (15, 20)},
                  'sea': {'tobase': (5, 5)}
                  }
 
--- RPGGame/npc.py
+++ RPGGame/npc.py
@@ -4,3 +4,14 @@
 @CreateTime   : 2018/8/15
 @Program      : 自己活动的NPC的功能实现。
 '''
+
+import pygame
+
+
+class NPCRole():
+    def __init__(self,image,):
+        self.npc_img = pygame.image.load(image).convert_alpha()
+
+
+    def auto_move(self):
+        pass
