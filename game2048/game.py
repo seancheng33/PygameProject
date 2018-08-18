730d527ff6ade09048e0cd705a85ecd745bec08d
seancheng <seancheng33@gmail.com>
2018-08-18 22:43:25 +0800

    game2048

---
+++ game2048/game.py
@@ -0,0 +1,22 @@
+'''
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2018/8/18
+@Program      : 联系用pygame制作一个2048的游戏，
+'''
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption('2048')
+
+while True:
+
+
+
+
+
+
+
+    pygame.display.update()
