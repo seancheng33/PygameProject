a2e5aa400dfa9f9dd7b698e265501ed29102eddc
seancheng <seancheng33@gmail.com>
2018-08-19 22:45:57 +0800

    game2048背景和退出事件

--- game2048/game.py
+++ game2048/game.py
@@ -5,7 +5,7 @@
 @Program      : 联系用pygame制作一个2048的游戏，
 '''
 
-import pygame
+import pygame, sys
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
@@ -13,9 +13,16 @@ pygame.display.set_caption('2048')
 
 while True:
 
+    screen.fill((127, 89, 127))
 
-
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit(0)
 
 
 
