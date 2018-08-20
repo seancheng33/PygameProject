2647071edd45190666414076df4e351cf46d276b
seancheng33 <seancheng33@gmail.com>
2018-08-20 14:15:21 +0800

    全局变量的准备

--- game2048/game.py
+++ game2048/game.py
@@ -8,15 +8,47 @@
 import pygame
 import sys
 
-initArray = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]  # 初始化的游戏数组
+COLORDICT = {'white': (255, 255, 255),  # 白色
+             'ivory': (255, 255, 240),  # 象牙色
+             'yellow': (255, 255, 0),  # 黄色
+             'seashell': (255, 245, 238),  # 海贝色
+             'bisque': (255, 228, 196),  # 橘黄色
+             'gold': (255, 215, 0),  # 金色
+             'pink': (255, 192, 203),  # 粉红色
+             'lightpink': (255, 182, 193),  # 亮粉红色
+             'orange': (255, 165, 0),  # 橙色
+             'coral': (255, 127, 80),  # 珊瑚色
+             'tomato': (255, 99, 71),  # 番茄色
+             'magenta': (255, 0, 255),  # 洋红色
+             'wheat': (245, 222, 179),  # 小麦色
+             'violet': (238, 130, 238),  # 紫罗兰色
+             'silver': (192, 192, 192),  # 银白色
+             'brown': (165, 42, 42),  # 棕色
+             'gray': (128, 128, 128),  # 灰色
+             'olive': (128, 128, 0),  # 橄榄色
+             'purple': (128, 0, 128),  # 紫色
+             'turquoise': (64, 224, 208),  # 绿宝石色
+             'seagreen': (46, 139, 87),  # 海洋绿色
+             'cyan': (0, 255, 255),  # 青色
+             'green': (0, 128, 0),  # 纯绿色
+             'blue': (0, 0, 255),  # 纯蓝色
+             'darkblue': (0, 0, 139),  # 深蓝色
+             'navy': (0, 0, 128),  # 海军蓝色
+             'black': (0, 0, 0),  # 纯黑色
+             }
+TITLSIZE = 80
+
+WINDOW_BLOCK_NUM = 4
+
+initArray = [0 for i in range(WINDOW_BLOCK_NUM) for j in range(WINDOW_BLOCK_NUM)]  # 初始化的游戏数组
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('2048')
+pygame.display.set_caption('游戏2048                  ver 1.0 Program By Sean Cheng')
 
 while True:
 
-    screen.fill((128, 128, 0))
+    screen.fill(COLORDICT['bisque'])
 
     # 五条横线
     pygame.draw.line(screen, (0, 0, 0), (100, 100), (500, 100), 2)
@@ -31,6 +63,11 @@ while True:
     pygame.draw.line(screen, (0, 0, 0), (300, 100), (300, 500), 2)
     pygame.draw.line(screen, (0, 0, 0), (400, 100), (400, 500), 2)
     pygame.draw.line(screen, (0, 0, 0), (500, 100), (500, 500), 2)
+
+    for i in range(initArray):
+        for j in range(initArray[0]):
+            # tile = pygame.draw.rect(screen, COLORDICT['silver'], ((TITLSIZE+10)*i+100, (TITLSIZE+10)*i+100, TITLSIZE, TITLSIZE))
+            pass
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
