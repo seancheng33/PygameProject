a5382bb05439fd8dcd29d4fa5655fd503b0a983f
seancheng33 <seancheng33@gmail.com>
2018-08-29 16:27:22 +0800

    添加cocos2d的练习

---
+++ cocos2d_demo/demo01.py
@@ -0,0 +1,26 @@
+"""
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2018/8/29
+@Program      : cocos2d的入门练习，根据官网内容
+"""
+import cocos
+
+class HelloWorld(cocos.layer.Layer):
+    def __init__(self):
+        super(HelloWorld, self).__init__()
+        label = cocos.text.Label(
+            'Cocos2d 测试练习',
+            font_name='SimHei',
+            font_size=32,
+            anchor_x='center',
+            anchor_y='center'
+        )
+        label.position = 320, 240
+        self.add(label)
+
+
+cocos.director.director.init()
+hello_layer = HelloWorld()
+main_scene = cocos.scene.Scene(hello_layer)
+cocos.director.director.run(main_scene)
