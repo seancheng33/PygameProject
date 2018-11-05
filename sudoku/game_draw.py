ef74fd47060dc4967dbd051191665a8342e2027b
seancheng33 <seancheng33@gmail.com>
2018-11-05 17:50:01 +0800

    数字的统计显示

--- sudoku/game_draw.py
+++ sudoku/game_draw.py
@@ -11,6 +11,15 @@ from sudoku.SetVar import SetVar
 setting = SetVar()
 
 
+def statistics(gameArray):
+    # 统计游戏数组中各个数字出现的次数。
+    num_dict = {}
+    for item in gameArray:
+        for num in item:
+            num_dict[num] = num_dict.get(num, 0)+1
+    return num_dict
+
+
 def draw_background(screen):
     # 背景板颜色
     screen.fill(setting.COLOR_DICT['ivory'])
@@ -33,6 +42,47 @@ def draw_background(screen):
     titleRect.topleft = 650, 50
     screen.blit(titleText, titleRect)
 
+    num1Text = setting.TEXTFONT.render('1', True, setting.COLOR_DICT['black'])
+    num2Text = setting.TEXTFONT.render('2', True, setting.COLOR_DICT['black'])
+    num3Text = setting.TEXTFONT.render('3', True, setting.COLOR_DICT['black'])
+    num4Text = setting.TEXTFONT.render('4', True, setting.COLOR_DICT['black'])
+    num5Text = setting.TEXTFONT.render('5', True, setting.COLOR_DICT['black'])
+    num6Text = setting.TEXTFONT.render('6', True, setting.COLOR_DICT['black'])
+    num7Text = setting.TEXTFONT.render('7', True, setting.COLOR_DICT['black'])
+    num8Text = setting.TEXTFONT.render('8', True, setting.COLOR_DICT['black'])
+    num9Text = setting.TEXTFONT.render('9', True, setting.COLOR_DICT['black'])
+
+    num1Rect = num1Text.get_rect()
+    num2Rect = num2Text.get_rect()
+    num3Rect = num3Text.get_rect()
+    num4Rect = num4Text.get_rect()
+    num5Rect = num5Text.get_rect()
+    num6Rect = num6Text.get_rect()
+    num7Rect = num7Text.get_rect()
+    num8Rect = num8Text.get_rect()
+    num9Rect = num9Text.get_rect()
+
+    num1Rect.topleft = 620, 520
+    num2Rect.topleft = 650, 520
+    num3Rect.topleft = 680, 520
+    num4Rect.topleft = 710, 520
+    num5Rect.topleft = 740, 520
+    num6Rect.topleft = 770, 520
+    num7Rect.topleft = 800, 520
+    num8Rect.topleft = 830, 520
+    num9Rect.topleft = 860, 520
+
+    screen.blit(num1Text, num1Rect)
+    screen.blit(num2Text, num2Rect)
+    screen.blit(num3Text, num3Rect)
+    screen.blit(num4Text, num4Rect)
+    screen.blit(num5Text, num5Rect)
+    screen.blit(num6Text, num6Rect)
+    screen.blit(num7Text, num7Rect)
+    screen.blit(num8Text, num8Rect)
+    screen.blit(num9Text, num9Rect)
+
+
 
 def draw_tile(screen, color, start_posx, start_posy, tile_width, tile_height):
     # 将这个rect给return出去，是为了方便下面的文字可以获取这个rect的信息，做对齐用
@@ -42,7 +92,7 @@ def draw_tile(screen, color, start_posx, start_posy, tile_width, tile_height):
 def draw_gameArray(screen, gameArray):
     posx,posy = pygame.mouse.get_pos()
     pressed = pygame.mouse.get_pressed()
-    selected = False
+
     for row in range(len(gameArray)):
         for col in range(len(gameArray[row])):
             if gameArray[row][col] != '0':
@@ -56,12 +106,12 @@ def draw_gameArray(screen, gameArray):
 
             else:
                 # 这里是绘制元素为零的贴片
-                tile = draw_tile(screen, setting.COLOR_DICT['yellow'], 60 * col + 30 + 6, 60 * row + 30 + 6,
+                tile = draw_tile(screen, setting.COLOR_DICT['ivory'], 60 * col + 30 + 6, 60 * row + 30 + 6,
                                  48, 48)
                 # 元素为零的贴片，需要可以点击，然后可以填充数字
                 if tile.collidepoint(posx, posy):
                     for event in pressed:
                         if event == 1:
-                            selected = True
+                            pass
 
-
+    num1Rect = pygame.draw.rect(screen, setting.COLOR_DICT['orange'], (620, 510, 10, -80))
