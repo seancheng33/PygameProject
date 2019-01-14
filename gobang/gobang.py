fa11d40fc024f8d5017edcd9e7c504690de3824d
seancheng33 <seancheng33@gmail.com>
2019-01-14 15:44:46 +0800

    五子棋落子基本功能完成，可以人人对弈。

--- gobang/gobang.py
+++ gobang/gobang.py
@@ -12,9 +12,11 @@ WHITE = 255, 255, 255
 BLACK = 0, 0, 0
 ORANGE = 255, 165, 0
 GRAY = 200, 200, 200
+RED = 255,0,0
 
 
 def draw_chess(screen, chess_color, posx, posy):
+    # 绘制棋子，传入的参数包括棋子的颜色和位置
     pygame.draw.circle(screen, chess_color, (posx, posy), 12)
 
 
@@ -49,19 +51,75 @@ def draw_player_icon(screen):
     pygame.draw.rect(screen, GRAY, (670, 445, 100, 100))
 
 
+def game_win(chess_array, row, col):
+    # 先获取目前下的棋子位置，然后再判断四个可能有连成一线的方向，如果有一个方向达成5颗棋子或以上的目标，就报游戏胜利。
+    chess = chess_array[row][col]
+
+    # 判断竖
+    same_chess = 0
+    for i in range(5):
+        # 需要控制下标越界的问题
+        if (col + i) < 15 and chess_array[row][col + i] == chess:
+            same_chess += 1
+        if (col - i) > -1 and chess_array[row][col - i] == chess:
+            same_chess += 1
+        if same_chess > 5:
+            return True
+
+    # 判断横
+    same_chess = 0
+    for i in range(5):
+        # 需要控制下标越界的问题
+        if (row + i) < 15 and chess_array[row + i][col] == chess:
+            same_chess += 1
+        if (row - i) > -1 and chess_array[row - i][col] == chess:
+            same_chess += 1
+        if same_chess > 5:
+            return True
+
+    # 判断左斜
+    same_chess = 0
+    for i in range(5):
+        # 需要控制下标越界的问题
+        if (col + i) < 15 and (row + i) < 15 and chess_array[row + i][col + i] == chess:
+            same_chess += 1
+        if (col - i) > -1 and (row - i) > -1 and chess_array[row - i][col - i] == chess:
+            same_chess += 1
+        if same_chess > 5:
+            return True
+
+    # 判断右斜
+    same_chess = 0
+    for i in range(5):
+        # 需要控制下标越界的问题
+        if (col + i) < 15 and (row - i) > -1 and chess_array[row - i][col + i] == chess:
+            same_chess += 1
+        if (col - i) > -1 and (row + i) < 15 and chess_array[row + i][col - i] == chess:
+            same_chess += 1
+        if same_chess > 5:
+            return True
+
+    return False
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('GoBang version 0.4 --Program by seancheng')
 
+    win_font = pygame.font.Font(None, 120)
+    win_str = ''
+
     chess_array = [[0 for i in range(15)] for j in range(15)]  # 存储棋子的状态
-    blackdown = True  # 当前是否为黑子下，这个判断条件有多种用途，用在判断现在改谁下子，以及谁下子后获得了胜利。
+    isblack = True  # 当前是否为黑子下，这个判断条件有多种用途，用在判断现在是谁落子，以及谁落子后获得了胜利。
+    iswin = False
 
     while True:
-        screen.fill(ORANGE)
+        screen.fill(ORANGE)  # 绘制背景的颜色
         draw_player_icon(screen)
         draw_chessboard(screen)
 
+        # 绘制出游戏中的各棋子位置
         for col in range(len(chess_array)):
             for row in range(len(chess_array[col])):
                 if chess_array[row][col] == 1:
@@ -89,14 +147,26 @@ def main():
                 row = (mouseX - 150 - 15) // 35 + 1
                 col = (mouseY - 55 - 15) // 35 + 1
                 # row和col必须是在0~14之间，不然就会报列表越界异常。
-                if -1 < row < 15 and -1 < col < 15 and chess_array[row][col] == 0:
-                    if blackdown:
+                if not iswin and -1 < row < 15 and -1 < col < 15 and chess_array[row][col] == 0:
+                    if isblack:
                         chess_array[row][col] = 1
-                        blackdown = False
+                        if game_win(chess_array, row, col):
+                            win_str = 'black win!'
+                            iswin = True
+                        isblack = False
                     else:
                         chess_array[row][col] = 2
-                        blackdown = True
-                # print(row, col)
+                        if game_win(chess_array, row, col):
+                            win_str = 'white win!'
+                            iswin = True
+                        isblack = True
+
+        # 如果游戏胜利，就显示胜利的画面
+        if iswin:
+            winstr = win_font.render(win_str, True, RED)
+            winstrRect = winstr.get_rect()
+            winstrRect.center = 400, 300
+            screen.blit(winstr, winstrRect)
 
         pygame.time.Clock().tick(150)
         pygame.display.update()
